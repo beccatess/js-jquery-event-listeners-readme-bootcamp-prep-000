f9b30c68366106a561fd5f2f433c997e1663ef15
Rebecca Kaplan <beccatess@gmail.com>
2017-09-15 15:59:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,19 @@
 $(document).ready(function(){
 
 // call functions here
+function window.getIt(){$('p')on('click', function(){alert('Hey!')}
+
+)}
+
+function frameIt(){$('img')on('load', function(){
+img.append('.tasty')
+}))
+
+}
+
+function pressIt(){ $('input')on('keydown', function(){if(e.detail === "g") {alert('You pressed G')}}
+)
+
+}
 
 });
